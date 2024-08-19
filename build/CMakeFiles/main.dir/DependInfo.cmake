5edc7e139ad77831ba9506c3daf13b33d399f7bc
Angelo <00550162@ufrgs.br>
2024-08-18 22:03:10 -0300

    texturas nao esticadas, modelo complexo e artefatos espalhados

---
+++ build/CMakeFiles/main.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/angel/Downloads/OpenGL_Raider2/OpenGL_Raider-main/src/glad.c" "CMakeFiles/main.dir/src/glad.c.obj" "gcc" "CMakeFiles/main.dir/src/glad.c.obj.d"
+  "C:/Users/angel/Downloads/OpenGL_Raider2/OpenGL_Raider-main/src/main.cpp" "CMakeFiles/main.dir/src/main.cpp.obj" "gcc" "CMakeFiles/main.dir/src/main.cpp.obj.d"
+  "C:/Users/angel/Downloads/OpenGL_Raider2/OpenGL_Raider-main/src/stb_image.cpp" "CMakeFiles/main.dir/src/stb_image.cpp.obj" "gcc" "CMakeFiles/main.dir/src/stb_image.cpp.obj.d"
+  "C:/Users/angel/Downloads/OpenGL_Raider2/OpenGL_Raider-main/src/textrendering.cpp" "CMakeFiles/main.dir/src/textrendering.cpp.obj" "gcc" "CMakeFiles/main.dir/src/textrendering.cpp.obj.d"
+  "C:/Users/angel/Downloads/OpenGL_Raider2/OpenGL_Raider-main/src/tiny_obj_loader.cpp" "CMakeFiles/main.dir/src/tiny_obj_loader.cpp.obj" "gcc" "CMakeFiles/main.dir/src/tiny_obj_loader.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
